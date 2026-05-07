479918923fd8520f84694666d0d5af2d1f59e345
M DEEMANTH HEBBAR <mdeemanthhebbar@gmail.com>
2026-05-07 20:31:22 +0530

    Move Firebase secrets to .env file and update server.js to use environment variables

--- notification_server/server.js
+++ notification_server/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const admin = require("firebase-admin");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// LOAD FIREBASE PRIVATE KEY
-const serviceAccount = require("./smart-canteen-eae33-firebase-adminsdk-fbsvc-e30c31723a.json");
+// LOAD FIREBASE PRIVATE KEY FROM ENV
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
